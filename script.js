1cd9fce437efea49d76ab155b254177ad17b2841
Rodrigo Longoria <ra.longoria86@gmail.com>
2022-04-06 21:18:39 -0500

    Github repo link and date added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById("date").innerHTML = today;
+
 
 let users =[];
 
